f98d08676168cca337e46b2b019f248a35733d50
Borodur <matt.sol@o2.pl>
2022-03-02 09:47:42 +0100

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,15 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+/**@serial
+ * @author W S M */
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello Java world\n");
+        // write your code here
+        /* To jest
+        komentarz
+        wielolinijkowy
+         */
+    }
+}
