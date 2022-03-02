eb7c54eb74393c68fc56be4ca3cb6a7bf0ee8801
Borodur <matt.sol@o2.pl>
2022-03-02 10:14:41 +0100

    Zrealizowano 7c
    Ala ma kota - każdy wyraz wypisany od nowej linijki

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,9 +5,9 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
+	System.out.println("Ala");
+        System.out.println("ma");
+        System.out.println("kota");
         // write your code here
         /* To jest
         komentarz
