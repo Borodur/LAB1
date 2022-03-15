f5323cd44441511c0441e95b9ec08a3d708b9c0f
Borodur <matt.sol@o2.pl>
2022-03-15 12:34:59 +0100

    Initial commit: zrealizowano punkt 7 z podpunktami d i e, program wyświetla przypisane dane.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,11 +5,13 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Ala\n");
-    System.out.print("Ma\n");
-    System.out.print("Kota\n");
+	int a = 3;
+    double b = 4.21;
+    String s="jakiś tekst";
+            System.out.printf("a = %d, b = %f, s = %s %n",a,b,s);
         // write your code here
         /* To jest
+        komentarz
         wielolinijkowy
          */
     }
