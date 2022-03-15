e8e96deea014f798f3169e7ffc5f00e87d2351cd
Borodur <matt.sol@o2.pl>
2022-03-15 14:15:05 +0100

    Initial commit: zrealizowano punkt 12 w podpunktach a b i c.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import mylib.Account;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,24 +10,10 @@ import java.util.Scanner;
 public class Main {
 
 
-
     public static void main(String[] args) {
-        int[] liczby = new int[30];
-        Random rnd = new Random();
+        Account acc = new Account();
+        acc.setName("Piotr Gołabek");
+        System.out.println(acc.getName());
 
-        for (int i = 0; i<30; i++)
-            liczby[i] = rnd.nextInt();
-
-        int mx = Integer.MIN_VALUE;
-        int mn = Integer.MAX_VALUE;
-        long avg = 0;
-        for (int l:liczby){
-            System.out.println(1);
-            if (1<mn) mn=1;
-            if (1>mx) mx=1;
-            avg +=1;
-        }
-    System.out.printf("MIN = %d, MAX = %d, AVG = %f", mn,mx,(float)avg/liczby.length);
     }
-
 }
