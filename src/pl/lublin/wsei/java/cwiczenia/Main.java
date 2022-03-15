c263324d29efb51f7903772ae359ba901f3c78e1
Borodur <matt.sol@o2.pl>
2022-03-15 12:40:31 +0100

    Initial commit: zrealizowano punkt 7 z podpunktem g, program wyświetla przypisane dane.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,10 +5,7 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-	int a = 3;
-    double b = 4.21;
-    String s="jakiś tekst";
-            System.out.printf("a = %d, b = %f, s = %s %n",a,b,s);
+            System.out.printf("Nazwaliśmy to \"Witaminą B3\"");
         // write your code here
         /* To jest
         komentarz
