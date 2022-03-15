79fd52f9ac8fdb46e74f4bc2ff53ed8f79c7f7a0
Borodur <matt.sol@o2.pl>
2022-03-15 14:33:21 +0100

    Initial commit: zrealizowano punkt 12 w podpunkcie e i f.

--- src/mylib/Account.java
+++ src/mylib/Account.java
@@ -1,13 +1,16 @@
 package mylib;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Account {
     private String name;
 
     public void setName(String name) {
-        this.name = name;
+        this.name = StringUtils.capitalize(name);
     }
     public String getName() {
         return name;
     }
+
 
 }
--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,18 +2,15 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import mylib.Account;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**@serial
  * @author W S M */
 public class Main {
 
 
     public static void main(String[] args) {
-        Account acc = new Account();
-        acc.name = "Piotr Gołabek";
-        System.out.println(acc.getName());
 
+        Account acc = new Account();
+        acc.setName("piotr gołabek");
+        System.out.println(acc.getName());
     }
 }
