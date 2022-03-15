9e2ab744fe4d983e7a21d41714d73e4da9841db7
Borodur <matt.sol@o2.pl>
2022-03-15 12:52:03 +0100

    Initial commit: zrealizowano punkt 9, program wyświetla przypisane dane.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -9,15 +9,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num1, num2;
-        System.out.print("Podaj pierwszą liczbę: ");
-        num1 = input.nextInt();
-        System.out.print("Podaj drugą liczbę: ");
-        num2 = input.nextInt();
-        System.out.printf("Wynik dodawania %d + %d = %d%n",num1,num2,num1+num2);
-        // write your code here
+        do {
+            System.out.print("Podaj pierwszą liczbę: ");
+            num1 = input.nextInt();
+            System.out.print("Podaj drugą liczbę: ");
+            num2 = input.nextInt();
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+            // write your code here
         /* To jest
         komentarz
         wielolinijkowy
          */
+        } while ((num1 != 0) && (num2 != 0));
     }
 }
