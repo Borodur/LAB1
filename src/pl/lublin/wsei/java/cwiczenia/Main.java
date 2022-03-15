3b3d6ef48b2de37a9f6a6c853d22e5d107c7b409
Borodur <matt.sol@o2.pl>
2022-03-15 12:21:51 +0100

    Initial commit: zrealizowano punkt 4, program wyświetla wpisany tekst.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,7 +5,7 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Java World\n")
+	System.out.println("Hello Java World\n");
         // write your code here
         /* To jest
         komentarz
