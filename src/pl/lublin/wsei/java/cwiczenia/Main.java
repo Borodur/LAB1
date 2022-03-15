de57b85fc5a057da6830c4dffd073bcdcc0c11b2
Borodur <matt.sol@o2.pl>
2022-03-15 12:42:59 +0100

    Initial commit: zrealizowano punkt 7 z podpunktem g, program wyświetla przypisane dane.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,7 +5,9 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-            System.out.printf("Nazwaliśmy to \"Witaminą B3\"");
+            System.out.printf("alfa\tsin(alfa)\n");
+            for (int i = 0; i<370; i+=10)
+                System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
         // write your code here
         /* To jest
         komentarz
